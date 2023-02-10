4565a4d1455da6493a89d180b025c7dac5328a92
Leonngc <nicolasleongoncalvescorrea@gmail.com>
2023-02-10 12:16:02 -0300

    Páginas home, about e order semi concluídas

---
+++ .history/scripts/menu_20230210114512.js
@@ -0,0 +1,12 @@
+//CONFIGURATIONS OF MEN
+
+ menu = document.getElementById('menu-burguer');
+const linkMenu = document.getElementById('links-menu');
+
+menu.addEventListener('click', event =>{
+    if(linkMenu.style.display == 'none'){
+    linkMenu.style.display = 'block'} else{
+        linkMenu.style.display = 'none'
+    }
+})
+
---
+++ .history/scripts/menu_20230210115433.js
@@ -0,0 +1,10 @@
+constmenu = document.getElementById('menu-burguer');
+const linkMenu = document.getElementById('links-menu');
+
+menu.addEventListener('click', event =>{
+    if(linkMenu.style.display == 'none'){
+    linkMenu.style.display = 'block'} else{
+        linkMenu.style.display = 'none'
+    }
+})
+
